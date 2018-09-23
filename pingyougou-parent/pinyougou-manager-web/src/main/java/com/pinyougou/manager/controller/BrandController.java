47637703621b44ed85155c793b4f83a3ba4f968d
admin <1179729485@qq.com>
2018-09-23 09:04:24 +0800

    运营商后台广告管理、网站前台广告展示、springDataRedis网站首页广告缓存-ok

--- pingyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pingyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
